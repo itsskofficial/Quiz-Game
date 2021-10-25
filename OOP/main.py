43f3a0cf2f356f8e04cd274e8aedd043a6490b92
SK <sarthakkarandikar03@gmail.com>
2021-10-25 16:21:43 +0530

    Commiting final changes

--- OOP/main.py
+++ OOP/main.py
@@ -4,15 +4,18 @@ from quiz_brain import *
 from art import *
 
 print(logo)
-print("\nWelcome to Quiz Game!")
+print("Welcome to Quiz Game!\n")
 question_bank = []
-i = 0
-for key, value in question_data[i].items():
-    question = Question(key, value)
+
+for item in question_data:
+    ques = item["text"]
+    ans = item["answer"]
+    question = Question(ques, ans)
     question_bank.append(question)
-    i += 1
 quiz = Quiz(question_bank)
 
 
 while quiz.question_remaining() == True:
     quiz.ask_question()
+
+print("\nThanks for playing quiz game!")
